2ce338e5a01a8ca2eab3d82d28cd03815312fc56
GeorgeaLima <georgea_nl@yahoo.com.br>
2025-03-20 22:08:43 -0300

    loginfailPage added

--- cypress/e2e/user.cy.js
+++ cypress/e2e/user.cy.js
@@ -3,15 +3,17 @@ import LoginPage from '../pages/loginPage'
 import DashboardPage  from '../pages/dashboardPage'
 import MenuPage from '../pages/menuPage'
 import MyinfoPage from '../pages/myinfoPage'
+import LoginFailPage from '../pages/loginfailPage'
 
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
 const myinfoPage = new MyinfoPage()
+const loginfailPage = new LoginFailPage()
 
 describe('Orange HRM tests', () => {
      
-  it.only('User Info Update- Success', () => {
+  it('User Info Update- Success', () => {
     loginPage.acessLoginPage()
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
 
@@ -19,7 +21,7 @@ describe('Orange HRM tests', () => {
     
     menuPage.accessMyInfo()
 
-    myinfoPage.fillpersonalDetails ('Dino', 'Silva', 'Sauro')
+    myinfoPage.fillpersonalDetails('Dino', 'Silva', 'Sauro')
     myinfoPage.fillEmployeeDetails('dinossaur', '66666', '7788991','2025-12-25', '1976-11-21')
     myinfoPage.fillStatus()
     myinfoPage.saveForm()
@@ -27,11 +29,10 @@ describe('Orange HRM tests', () => {
   })
 
   it('Login- Fail', () => {
-    cy.visit('/auth/login')
-    cy.get(selectorsList.usernameField).type(userData.userFail.username)
-    cy.get(selectorsList.passwordField).type(userData.userFail.password)
-    cy.get(selectorsList.loginButton).click()
-    cy.get(selectorsList.wrongCredentialAlert)
+    
+    loginfailPage.acessLoginPage()
+    loginfailPage.loginWithUser('dino', 'sauro')
+
 
   })
 
